e09b6eddc0b942acafd2b9e69b96292ce2f6b3cd
David MH Ryu <myeonghan12@gmail.com>
2021-08-22 23:32:41 +0900

    make class

--- app.ts
+++ app.ts
@@ -47,11 +47,11 @@ class Api {
 		this.ajax = new XMLHttpRequest();
 	}
 
-	getRequest<AjaxResponse>(): AjaxResponse {
+	protected getRequest<AjaxResponse>(): AjaxResponse {
 		this.ajax.open('GET', this.url, false);
 		this.ajax.send();
 
-		return JSON.parse(ajax.response);
+		return JSON.parse(this.ajax.response);
 	}
 }
 
@@ -65,14 +65,6 @@ class NewsDetailApi extends Api {
 	getData(): NewsDetail {
 		return this.getRequest<NewsDetail>();
 	}
-}
-
-// 유니언 기능 '|' -> 제네릭 기능(타입 가드 힘들 때)
-function getData<AjaxResponse>(url: string): AjaxResponse {
-	ajax.open('GET', url, false);
-	ajax.send();
-
-	return JSON.parse(ajax.response);
 }
 
 function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
@@ -92,6 +84,9 @@ function updateView(html: string): void {
 }
 
 function newsFeed(): void {
+	// newsfeed instance
+	const api = new NewsFeedApi(NEWS_URL);
+
 	// 매번 getData 하지 않도록
 	let newsFeed: NewsFeed[] = store.feeds;
 	const newsList = [];
@@ -121,7 +116,7 @@ function newsFeed(): void {
     `;
 
 	if (newsFeed.length === 0) {
-		newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
+		newsFeed = store.feeds = makeFeeds(api.getData());
 	}
 
 	for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -163,7 +158,8 @@ function newsFeed(): void {
 
 function newsDetail(): void {
 	const id = location.hash.substr(7);
-	const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id));
+	const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
+	const newsContent = api.getData();
 
 	const template = `
     <div class="bg-gray-600 min-h-screen pb-8">
